2724c43e10f1b9ec20b32e96ed94278705f1b39e
FernandoJCFilho <Kolumbia3000@gmail.com>
2025-10-17 20:41:27 -0300

    Calculadora geometrica e adicao dela ao menu

--- exercicios_linguagem/src/exercicios_linguagem/CalculadoraGeometrica.java
+++ exercicios_linguagem/src/exercicios_linguagem/CalculadoraGeometrica.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package exercicios_linguagem;
 
-/**
- *
- * @author aluno
- */
 public class CalculadoraGeometrica {
-    
+    public static double calcularAreaQuadrado(double lado){
+        return lado * lado;
+    }
 }
