40dd213432ee9c50b4b608ca731a83f7c289f7e3
FernandoJCFilho <Kolumbia3000@gmail.com>
2025-10-17 21:18:10 -0300

    convesor

--- exercicios_linguagem/src/exercicios_linguagem/ConversorTemperatura.java
+++ exercicios_linguagem/src/exercicios_linguagem/ConversorTemperatura.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package exercicios_linguagem;
 
-/**
- *
- * @author aluno
- */
 public class ConversorTemperatura {
-    
+    public static double celciusParaFahrenheit(double celsius){  
+        return celsius * 1.8 + 32;     
+    }
 }
